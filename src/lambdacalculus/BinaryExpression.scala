0c2d089c1ed926c804814627272e21177439fd14
keenlight <413535281@qq.com>
2015-05-24 21:28:26 +0800

    test commit

--- src/lambdacalculus/BinaryExpression.scala
+++ src/lambdacalculus/BinaryExpression.scala
@@ -1,14 +1,11 @@
 package lambdacalculus
 
-/**
- * Created by Administrator on 2015/5/23.
- */
-case class BinaryExpression(expr1 : Expression, op : String, expr2 : Expression) extends Expression{
+case class BinaryExpression(expr1: Expression, op: String, expr2: Expression) extends Expression {
 
-  override def toString() : String = {
+  override def toString: String = {
     expr1.toString + op + expr2.toString
   }
 
 }
 
-object BinaryExpression{}
+object BinaryExpression {}
